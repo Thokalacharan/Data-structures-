4354786f9e0b4c6163409c4b95dea12acc76141d
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-05-08 10:36:06 +0530

    Update Linearsearch.c

--- Linearsearch.c
+++ Linearsearch.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-
-// Function to perform linear search
-int linearSearch(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == key)
-            return i; 
-    }
-    return -1; 
-}
-
-int main() {
-    int arr[] = {12, 34, 54, 2, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int key = 54;
-    int result = linearSearch(arr, n, key);
-    if (result != -1)
-        printf("Element %d found at index %d\n", key, result);
+/*C Program to perform Linear Search*/
+#include<stdio.h>
+int linearsearch(int [], int, int);
+int main()
+{
+    int a[20], i, n, key, pos;
+    printf("Enter n:");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n",n);
+    for(i=0; i<n; i++)
+        scanf("%d",&a[i]);
+    printf("Enter the element to be searched:");
+    scanf("%d",&key);
+    pos = linearsearch(a, n, key);
+    if(pos==-1)
+        printf("Search is unsuccessful");
     else
-        printf("Element %d not found\n", key);
+        printf("Key is found at index: %d",pos);
     return 0;
 }
-    
+int linearsearch(int a[], int n, int key)
+{
+    int i;
+    for(i=0; i<n; i++)
+        if(a[i]==key)
+            return i;
+    return -1;
+}
